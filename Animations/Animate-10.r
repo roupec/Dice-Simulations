5f7fc68ee6d90a5655af149a3ecc037892f6a535
Debartha Paul <53690728+itsdebartha@users.noreply.github.com>
2022-09-09 12:24:17 +0530

    Create Animate-10.r
    Added the animation codes for the tenth and the last question

---
+++ Animations/Animate-10.r
@@ -0,0 +1,17 @@
+mean_vec<-NULL
+for(i in 1:20)
+{
+	x<-replicate(100000,rolls(i))
+	mean_vec<-c(mean_vec,mean(x))
+}
+mean_vec
+saveGIF(for(i in 1:20)
+{
+	plot(1:i,mean_vec[1:i],ylim=c(0,1),
+		main='Simulation',xlab='No. of throws',
+		ylab='Probability',type='l',
+		col='blue')
+},
+movie.name="Sim-10.gif",
+interval=0.2
+)
